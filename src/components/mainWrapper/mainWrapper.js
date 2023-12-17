b796a1a6cd6b7adf4ed659fe39f7bc13ae160e47
Yura <cb8593bc@gmail.com>
2023-12-17 11:48:36 +0100

    add default list

--- src/components/mainWrapper/mainWrapper.js
+++ src/components/mainWrapper/mainWrapper.js
@@ -4,21 +4,12 @@ import { observer } from "mobx-react-lite";
 import './mainWrapper.css';
 import {NamesComponent} from "../namesComponent/namesComponent";
 import {Filters} from "../filters/filters";
-import {names} from "../../store/names";
 
-const list =  names();
-console.log('*** names' ,list.names.length )
-console.log('*** names' ,list.names.push({}) )
-setTimeout(()=>{
-    list.names.push({})
-},5000);
 
-console.log('*** names' ,list.names.length )
 export const MainWrapper = observer(() => {
     return <div className='main-wrapper'>
         <Filters/>
-        {list.names.length}
-        <NamesComponent>
-        </NamesComponent>
+
+        <NamesComponent />
     </div>;
 })
