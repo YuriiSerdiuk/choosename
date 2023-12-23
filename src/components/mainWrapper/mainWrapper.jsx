f90eb00b29c20f127e23ae8b3bd56928ab0be44e
Yura <cb8593bc@gmail.com>
2023-12-23 23:05:18 +0100

    add app bar

--- src/components/mainWrapper/mainWrapper.jsx
+++ src/components/mainWrapper/mainWrapper.jsx
@@ -4,11 +4,13 @@ import { observer } from "mobx-react-lite";
 import './mainWrapper.css';
 import {NamesComponent} from "../namesComponent/namesComponent";
 import {Filters} from "../filters/filters";
+import MenuAppBar from "../app-bar/app-bar";
 
 
 export const MainWrapper = observer(() => {
     return <div className='main-wrapper'>
-        <Filters/>
+        {/*<Filters/>*/}
+        <MenuAppBar />
         <NamesComponent />
     </div>;
 })
--- src/constants/names.js
+++ src/constants/names.js
@@ -1,102 +1,639 @@
 export const namesList = [
-    {"name": "Марія", "country": "Греція"},
-    {"name": "Марта", "country": "Ізраїль"},
-    {"name": "Марина", "country": "Латвія"},
-    {"name": "Маргарита", "country": "Італія"},
-    {"name": "Меланія", "country": "Словенія"},
-    {"name": "Мілена", "country": "Чехія"},
-    {"name": "Майя", "country": "Іспанія"},
-    {"name": "Маргарет", "country": "Англія"},
-    {"name": "Марта", "country": "Польща"},
-    {"name": "Меган", "country": "Англія"},
-    {"name": "Міранда", "country": "Італія"},
-    {"name": "Мері", "country": "Англія"},
-    {"name": "Мейвіс", "country": "Ірландія"},
-    {"name": "Мелісса", "country": "Греція"},
-    {"name": "Моллі", "country": "Ірландія"},
-    {"name": "Моніка", "country": "Латвія"},
-    {"name": "Моргана", "country": "Уельс"},
-    {"name": "Магдалена", "country": "Італія"},
-    {"name": "Марсія", "country": "Римська імперія"},
-    {"name": "Максін", "country": "Латинська Америка"},
-    {"name": "Міа", "country": "Іспанія"},
-    {"name": "Мішель", "country": "Франція"},
-    {"name": "Міллісент", "country": "Англія"},
-    {"name": "Міша", "country": "Росія"},
-    {"name": "Марен", "country": "Данія"},
-    {"name": "Мадлен", "country": "Франція"},
-    {"name": "Мег", "country": "Греція"},
-    {"name": "Мередіт", "country": "Уельс"},
-    {"name": "Меггі", "country": "Ірландія"},
-    {"name": "Метта", "country": "Данія"},
-    {"name": "Меріль", "country": "Англія"},
-    {"name": "Мерседес", "country": "Іспанія"},
-    {"name": "Меделін", "country": "Іспанія"},
-    {"name": "Мейбел", "country": "Ірландія"},
-    {"name": "Мелінда", "country": "Греція"},
-    {"name": "Мартіна", "country": "Латвія"},
-    {"name": "Марлен", "country": "Німеччина"},
-    {"name": "Мінді", "country": "Ірландія"},
-    {"name": "Мейлзі", "country": "Ірландія"},
-    {"name": "Марлен", "country": "Німеччина"},
-    {"name": "Мелода", "country": "Греція"},
-    {"name": "Меріен", "country": "Англія"},
-    {"name": "Метільда", "country": "Італія"},
-    {"name": "Морі", "country": "Ірландія"},
-    {"name": "Мінні", "country": "Англія"},
-    {"name": "Марселла", "country": "Італія"},
-    {"name": "Меган", "country": "Англія"},
-    {"name": "Міріам", "country": "Ізраїль"},
-    {"name": "Малорі", "country": "Ірландія"},
-    {"name": "Маргот", "country": "Франція"},
-    {"name": "Меріон", "country": "Англія"},
-    {"name": "Мора", "country": "Ірландія"},
-    {"name": "Мередіт", "country": "Уельс"},
-    {"name": "Міган", "country": "Ірландія"},
-    {"name": "Маркета", "country": "Чехія"},
-    {"name": "Мія", "country": "Іспанія"},
-    {"name": "Муріель", "country": "Франція"},
-    {"name": "Манді", "country": "Англія"},
-    {"name": "Мінерва", "country": "Римська імперія"},
-    {"name": "Меріон", "country": "Англія"},
-    {"name": "Маурін", "country": "Ірландія"},
-    {"name": "Марвел", "country": "Англія"},
-    {"name": "Мейрі", "country": "Англія"},
-    {"name": "Міриам", "country": "Ізраїль"}
+  {"name": "Emma"},
+  {"name": "Olivia"},
+  {"name": "Ava"},
+  {"name": "Isabella"},
+  {"name": "Sophia"},
+  {"name": "Mia"},
+  {"name": "Amelia"},
+  {"name": "Harper"},
+  {"name": "Evelyn"},
+  {"name": "Abigail"},
+  {"name": "Emily"},
+  {"name": "Ella"},
+  {"name": "Scarlett"},
+  {"name": "Grace"},
+  {"name": "Chloe"},
+  {"name": "Camila"},
+  {"name": "Luna"},
+  {"name": "Avery"},
+  {"name": "Mila"},
+  {"name": "Aria"},
+  {"name": "Eleanor"},
+  {"name": "Hannah"},
+  {"name": "Addison"},
+  {"name": "Eliana"},
+  {"name": "Aubrey"},
+  {"name": "Natalie"},
+  {"name": "Leah"},
+  {"name": "Sofia"},
+  {"name": "Ashley"},
+  {"name": "Aurora"},
+  {"name": "Madison"},
+  {"name": "Bella"},
+  {"name": "Aaliyah"},
+  {"name": "Lucy"},
+  {"name": "Aria"},
+  {"name": "Stella"},
+  {"name": "Genesis"},
+  {"name": "Zoe"},
+  {"name": "Alexa"},
+  {"name": "Paisley"},
+  {"name": "Hazel"},
+  {"name": "Ellie"},
+  {"name": "Violet"},
+  {"name": "Lily"},
+  {"name": "Zoey"},
+  {"name": "Aurora"},
+  {"name": "Savannah"},
+  {"name": "Audrey"},
+  {"name": "Brooklyn"},
+  {"name": "Bella"},
+  {"name": "Aaliyah"},
+  {"name": "Skylar"},
+  {"name": "Claire"},
+  {"name": "Peyton"},
+  {"name": "Layla"},
+  {"name": "Lillian"},
+  {"name": "Nora"},
+  {"name": "Zoe"},
+  {"name": "Lucy"},
+  {"name": "Ruby"},
+  {"name": "Eva"},
+  {"name": "Scarlett"},
+  {"name": "Aubrey"},
+  {"name": "Isabella"},
+  {"name": "Leah"},
+  {"name": "Aurora"},
+  {"name": "Lily"},
+  {"name": "Hazel"},
+  {"name": "Violet"},
+  {"name": "Nova"},
+  {"name": "Emilia"},
+  {"name": "Everly"},
+  {"name": "Grace"},
+  {"name": "Emma"},
+  {"name": "Chloe"},
+  {"name": "Zoey"},
+  {"name": "Ella"},
+  {"name": "Mia"},
+  {"name": "Aria"},
+  {"name": "Eva"},
+  {"name": "Luna"},
+  {"name": "Mila"},
+  {"name": "Scarlett"},
+  {"name": "Layla"},
+  {"name": "Penelope"},
+  {"name": "Aurora"},
+  {"name": "Sofia"},
+  {"name": "Camila"},
+  {"name": "Avery"},
+  {"name": "Abigail"},
+  {"name": "Lily"},
+  {"name": "Zoe"},
+  {"name": "Mia"},
+  {"name": "Aria"},
+  {"name": "Luna"},
+  {"name": "Scarlett"},
+  {"name": "Hazel"},
+  {"name": "Aurora"},
+  {"name": "Mila"},
+  {"name": "Lily"},
+  {"name": "Chloe"},
+  {"name": "Ella"},
+  {"name": "Grace"},
+  {"name": "Avery"},
+  {"name": "Sofia"},
+  {"name": "Camila"},
+  {"name": "Eva"},
+  {"name": "Nova"},
+  {"name": "Scarlett"},
+  {"name": "Zoe"},
+  {"name": "Aria"},
+  {"name": "Lily"},
+  {"name": "Mia"},
+  {"name": "Chloe"},
+  {"name": "Ella"},
+  {"name": "Aurora"},
+  {"name": "Grace"},
+  {"name": "Luna"},
+  {"name": "Mila"},
+  {"name": "Hazel"},
+  {"name": "Avery"},
+  {"name": "Sofia"},
+  {"name": "Camila"},
+  {"name": "Eva"},
+  {"name": "Zoe"},
+  {"name": "Nova"},
+  {"name": "Aria"},
+  {"name": "Lily"},
+  {"name": "Scarlett"},
+  {"name": "Mia"},
+  {"name": "Chloe"},
+  {"name": "Ella"},
+  {"name": "Aurora"},
+  {"name": "Grace"},
+  {"name": "Avery"},
+  {"name": "Luna"},
+  {"name": "Mila"},
+  {"name": "Hazel"},
+  {"name": "Sofia"},
+  {"name": "Zoe"},
+  {"name": "Camila"},
+  {"name": "Nova"},
+  {"name": "Aria"},
+  {"name": "Lily"},
+  {"name": "Scarlett"},
+  {"name": "Ella"},
+  {"name": "Mia"},
+  {"name": "Aurora"},
+  {"name": "Grace"},
+  {"name": "Luna"},
+  {"name": "Avery"},
+  {"name": "Mila"},
+  {"name": "Hazel"},
+  {"name": "Zoe"},
+  {"name": "Sofia"},
+  {"name": "Camila"},
+  {"name": "Nova"}
 ];
 
 export const alphabetOrderNameList = {
-    а: [],
-    б: [],
-    в: [],
-    г: [],
-    ґ: [],
-    д: [],
-    е: [],
-    є: [],
-    ж: [],
-    з: [],
-    и: [],
-    і: [],
-    ї: [],
-    й: [],
-    к: [],
-    л: [],
-    м: [],
-    н: [],
-    о: [],
-    п: [],
-    р: [],
-    с: [],
-    т: [],
-    у: [],
-    ф: [],
-    х: [],
-    ц: [],
-    ч: [],
-    ш: [],
-    щ: [],
-    ь: [],
-    ю: [],
-    я: []
+  а: [],
+  б: [],
+  в: [],
+  г: [],
+  ґ: [],
+  д: [],
+  е: [],
+  є: [],
+  ж: [],
+  з: [],
+  и: [],
+  і: [],
+  ї: [],
+  й: [],
+  к: [],
+  л: [],
+  м: [],
+  н: [],
+  о: [],
+  п: [],
+  р: [],
+  с: [],
+  т: [],
+  у: [],
+  ф: [],
+  х: [],
+  ц: [],
+  ч: [],
+  ш: [],
+  щ: [],
+  ь: [],
+  ю: [],
+  я: []
 };
+
+export const namesList2 = [
+  {
+    m: {"name": "Liam"},
+    f: {"name": "Olivia"}
+  },
+  {
+    m: {"name": "Noah"},
+    f: {"name": "Emma"}
+  },
+  {
+    m: {"name": "Oliver"},
+    f: {"name": "Ava"}
+  },
+  {
+    m: {"name": "Elijah"},
+    f: {"name": "Isabella"}
+  },
+  {
+    m: {"name": "William"},
+    f: {"name": "Sophia"}
+  },
+  {
+    m: {"name": "James"},
+    f: {"name": "Mia"}
+  },
+  {
+    m: {"name": "Benjamin"},
+    f: {"name": "Amelia"}
+  },
+  {
+    m: {"name": "Lucas"},
+    f: {"name": "Harper"}
+  },
+  {
+    m: {"name": "Henry"},
+    f: {"name": "Evelyn"}
+  },
+  {
+    m: {"name": "Alexander"},
+    f: {"name": "Abigail"}
+  },
+  {
+    m: {"name": "Sebastian"},
+    f: {"name": "Emily"}
+  },
+  {
+    m: {"name": "Jack"},
+    f: {"name": "Ella"}
+  },
+  {
+    m: {"name": "Aiden"},
+    f: {"name": "Scarlett"}
+  },
+  {
+    m: {"name": "Owen"},
+    f: {"name": "Grace"}
+  },
+  {
+    m: {"name": "Caleb"},
+    f: {"name": "Chloe"}
+  },
+  {
+    m: {"name": "Matteo"},
+    f: {"name": "Camila"}
+  },
+  {
+    m: {"name": "Leo"},
+    f: {"name": "Luna"}
+  },
+  {
+    m: {"name": "Lincoln"},
+    f: {"name": "Avery"}
+  },
+  {
+    m: {"name": "Adam"},
+    f: {"name": "Mila"}
+  },
+  {
+    m: {"name": "Christopher"},
+    f: {"name": "Aria"}
+  },
+  {
+    m: {"name": "Isaac"},
+    f: {"name": "Eleanor"}
+  },
+  {
+    m: {"name": "Julian"},
+    f: {"name": "Hannah"}
+  },
+  {
+    m: {"name": "Mateo"},
+    f: {"name": "Addison"}
+  },
+  {
+    m: {"name": "David"},
+    f: {"name": "Eliana"}
+  },
+  {
+    m: {"name": "Joseph"},
+    f: {"name": "Aubrey"}
+  },
+  {
+    m: {"name": "Carter"},
+    f: {"name": "Natalie"}
+  },
+  {
+    m: {"name": "John"},
+    f: {"name": "Leah"}
+  },
+  {
+    m: {"name": "Jonathan"},
+    f: {"name": "Sofia"}
+  },
+  {
+    m: {"name": "Gabriel"},
+    f: {"name": "Ashley"}
+  },
+  {
+    m: {"name": "Jackson"},
+    f: {"name": "Aurora"}
+  },
+  {
+    m: {"name": "Daniel"},
+    f: {"name": "Madison"}
+  },
+  {
+    m: {"name": "Isaiah"},
+    f: {"name": "Bella"}
+  },
+  {
+    m: {"name": "Max"},
+    f: {"name": "Aaliyah"}
+  },
+  {
+    m: {"name": "Anthony"},
+    f: {"name": "Lucy"}
+  },
+  {
+    m: {"name": "Ezra"},
+    f: {"name": "Aria"}
+  },
+  {
+    m: {"name": "Lincoln"},
+    f: {"name": "Stella"}
+  },
+  {
+    m: {"name": "Andrew"},
+    f: {"name": "Genesis"}
+  },
+  {
+    m: {"name": "Nicholas"},
+    f: {"name": "Zoe"}
+  },
+  {
+    m: {"name": "Nathan"},
+    f: {"name": "Alexa"}
+  },
+  {
+    m: {"name": "Elias"},
+    f: {"name": "Paisley"}
+  },
+  {
+    m: {"name": "Hunter"},
+    f: {"name": "Hazel"}
+  },
+  {
+    m: {"name": "Christian"},
+    f: {"name": "Ellie"}
+  },
+  {
+    m: {"name": "Jack"},
+    f: {"name": "Violet"}
+  },
+  {
+    m: {"name": "Cameron"},
+    f: {"name": "Lily"}
+  },
+  {
+    m: {"name": "Evan"},
+    f: {"name": "Zoey"}
+  },
+  {
+    m: {"name": "Landon"},
+    f: {"name": "Aurora"}
+  },
+  {
+    m: {"name": "Adrian"},
+    f: {"name": "Savannah"}
+  },
+  {
+    m: {"name": "Kayden"},
+    f: {"name": "Audrey"}
+  },
+  {
+    m: {"name": "Miles"},
+    f: {"name": "Brooklyn"}
+  },
+  {
+    m: {"name": "Eli"},
+    f: {"name": "Bella"}
+  },
+  {
+    m: {"name": "Justin"},
+    f: {"name": "Aaliyah"}
+  },
+  {
+    m: {"name": "Hudson"},
+    f: {"name": "Skylar"}
+  },
+  {
+    m: {"name": "Easton"},
+    f: {"name": "Claire"}
+  },
+  {
+    m: {"name": "Jordan"},
+    f: {"name": "Peyton"}
+  },
+  {
+    m: {"name": "Aaron"},
+    f: {"name": "Layla"}
+  },
+  {
+    m: {"name": "Robert"},
+    f: {"name": "Lillian"}
+  },
+  {
+    m: {"name": "Jameson"},
+    f: {"name": "Nora"}
+  },
+  {
+    m: {"name": "Nicholas"},
+    f: {"name": "Zoe"}
+  },
+  {
+    m: {"name": "Santiago"},
+    f: {"name": "Lucy"}
+  },
+  {
+    m: {"name": "Leonardo"},
+    f: {"name": "Ruby"}
+  },
+  {
+    m: {"name": "Dominic"},
+    f: {"name": "Eva"}
+  },
+  {
+    m: {"name": "Jeremiah"},
+    f: {"name": "Scarlett"}
+  },
+  {
+    m: {"name": "Declan"},
+    f: {"name": "Aubrey"}
+  },
+  {
+    m: {"name": "Cole"},
+    f: {"name": "Isabella"}
+  },
+  {
+    m: {"name": "Grayson"},
+    f: {"name": "Leah"}
+  },
+  {
+    m: {"name": "Caleb"},
+    f: {"name": "Aurora"}
+  },
+  {
+    m: {"name": "Kai"},
+    f: {"name": "Lily"}
+  },
+  {
+    m: {"name": "John"},
+    f: {"name": "Hazel"}
+  },
+  {
+    m: {"name": "Beckett"},
+    f: {"name": "Violet"}
+  },
+  {
+    m: {"name": "Jordan"},
+    f: {"name": "Nova"}
+  },
+  {
+    m: {"name": "Oscar"},
+    f: {"name": "Emilia"}
+  },
+  {
+    m: {"name": "Zayden"},
+    f: {"name": "Everly"}
+  },
+  {
+    m: {"name": "Caden"},
+    f: {"name": "Grace"}
+  },
+  {
+    m: {"name": "Mateo"},
+    f: {"name": "Emma"}
+  },
+  {
+    m: {"name": "Maxwell"},
+    f: {"name": "Chloe"}
+  },
+  {
+    m: {"name": "Ian"},
+    f: {"name": "Zoey"}
+  },
+  {
+    m: {"name": "Ezekiel"},
+    f: {"name": "Ella"}
+  },
+  {
+    m: {"name": "Leo"},
+    f: {"name": "Mia"}
+  },
+  {
+    m: {"name": "Axel"},
+    f: {"name": "Aria"}
+  },
+  {
+    m: {"name": "Luca"},
+    f: {"name": "Eva"}
+  },
+  {
+    m: {"name": "Zane"},
+    f: {"name": "Luna"}
+  },
+  {
+    m: {"name": "Aiden"},
+    f: {"name": "Mila"}
+  },
+  {
+    m: {"name": "Eli"},
+    f: {"name": "Scarlett"}
+  },
+  {
+    m: {"name": "Collin"},
+    f: {"name": "Layla"}
+  },
+  {
+    m: {"name": "Xavier"},
+    f: {"name": "Penelope"}
+  },
+  {
+    m: {"name": "Peter"},
+    f: {"name": "Aurora"}
+  },
+  {
+    m: {"name": "Daniel"},
+    f: {"name": "Sofia"}
+  },
+  {
+    m: {"name": "Jace"},
+    f: {"name": "Camila"}
+  },
+  {
+    m: {"name": "Everett"},
+    f: {"name": "Avery"}
+  },
+  {
+    m: {"name": "Jaxson"},
+    f: {"name": "Abigail"}
+  },
+  {
+    m: {"name": "Preston"},
+    f: {"name": "Lily"}
+  },
+  {
+    m: {"name": "Damian"},
+    f: {"name": "Zoe"}
+  },
+  {
+    m: {"name": "Sawyer"},
+    f: {"name": "Mia"}
+  },
+  {
+    m: {"name": "Christian"},
+    f: {"name": "Aria"}
+  },
+  {
+    m: {"name": "Tucker"},
+    f: {"name": "Ella"}
+  },
+  {
+    m: {"name": "Silas"},
+    f: {"name": "Grace"}
+  },
+  {
+    m: {"name": "Arlo"},
+    f: {"name": "Avery"}
+  },
+  {
+    m: {"name": "Harrison"},
+    f: {"name": "Sofia"}
+  },
+  {
+    m: {"name": "George"},
+    f: {"name": "Camila"}
+  },
+  {
+    m: {"name": "Wyatt"},
+    f: {"name": "Aubrey"}
+  },
+  {
+    m: {"name": "Ezra"},
+    f: {"name": "Natalie"}
+  },
+  {
+    m: {"name": "Weston"},
+    f: {"name": "Leah"}
+  },
+  {
+    m: {"name": "Leon"},
+    f: {"name": "Aurora"}
+  },
+  {
+    m: {"name": "Brayden"},
+    f: {"name": "Lily"}
+  },
+  {
+    m: {"name": "Alex"},
+    f: {"name": "Zoe"}
+  },
+  {
+    m: {"name": "Bennett"},
+    f: {"name": "Madison"}
+  },
+  {
+    m: {"name": "Cristian"},
+    f: {"name": "Bella"}
+  },
+  {
+    m: {"name": "Tanner"},
+    f: {"name": "Aaliyah"}
+  },
+  {
+    m: {"name": "Knox"},
+    f: {"name": "Lucy"}
+  },
+  {
+    m: {"name": "Grayson"},
+    f: {"name": "Aria"}
+  },
+];
+
