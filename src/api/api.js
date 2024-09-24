aff2eb4b6945bf7ddd96ff91568b71cccabd6d85
Yura <cb8593bc@gmail.com>
2024-09-24 17:37:38 +0200

    configured API

--- src/api/api.js
+++ src/api/api.js
@@ -1,68 +1,77 @@
 import axios from "axios";
+import { endpoints } from '../constants/endpoints';
 
-import {endpoints} from '../constants/endpoints';
+const defaultHeaders = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+};
 
 class Api {
   instance = axios.create({
     baseURL: endpoints.URL,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
+    headers: defaultHeaders,
+    // timeout: 10000, // 10 секунд
   });
 
+  async request(method, url, dataOrParams, options) {
+    try {
+      return await this.instance[method](url, dataOrParams, options);
+    } catch (error) {
+      console.error(`Error during ${method.toUpperCase()} request to ${url}:`, error);
+      throw error;
+    }
+  }
+
   async get(url, params) {
-    return await this.instance.get(url, {params});
+    return await this.request('get', url, { params });
   }
 
   async post(url, data, options) {
-    return await this.instance.post(url, data, options);
+    return await this.request('post', url, data, options);
   }
 
   async put(url, data) {
-    return await this.instance.put(url, data);
+    return await this.request('put', url, data);
   }
 
-
   async delete(url, data) {
-    return await this.instance.delete(url, data);
+    return await this.request('delete', url, data);
   }
 
   async getSignIn(params) {
-    return await this.post(endpoints.SignIn, params);
+    return this.post(endpoints.SignIn, params);
   }
 
   async signUp(params) {
-    return await this.post(endpoints.SignUp, params);
+    return this.post(endpoints.SignUp, params);
   }
 
   async createNewList(params) {
-    return await this.post(endpoints.CreateNewList, params);
+    return this.post(endpoints.CreateNewList, params);
   }
 
   async getList(params) {
-    return await this.get(endpoints.GetList, params);
+    return this.get(endpoints.GetList, params);
   }
 
   async getLikedNames(params) {
-    return await this.get(endpoints.GetLikedName, params);
+    return this.get(endpoints.GetLikedName, params);
   }
 
-
   async getUnLikedNames(params) {
-    return await this.get(endpoints.GetUnLikedName, params);
+    return this.get(endpoints.GetUnLikedName, params);
   }
 
   async addLikedName(params) {
-    return await this.put(endpoints.AddLikedName, params);
+    return this.put(endpoints.AddLikedName, params);
   }
 
   async addUnlikedName(params) {
-    return await this.put(endpoints.AddUnlikedName, params);
+    return this.put(endpoints.AddUnlikedName, params);
   }
 
   async addNameToList(params) {
-    return await this.put(endpoints.AddNameToList, params);
+    return this.put(endpoints.AddNameToList, params);
   }
 }
 
